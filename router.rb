cbc7203b99255f0bd1f3d6f34b562b498c8d7000
Jesse Buchanan <jbinto@jbinto.ca>
2013-03-18 10:37:17 -0400

    Define valid commands (index and show)

--- router.rb
+++ router.rb
@@ -6,4 +6,12 @@ if ARGV.empty?
 	puts "Usage: [args]"
 else
 	puts "Arguments passed in: #{ARGV.join(" ")}"
+
+	if ARGV[0] == "index"
+    puts "Action: index"
+	elsif ARGV[0] == "show"
+    puts "Action: show"
+	else
+		puts "Unrecognized action. Please refer to USAGE."
+	end
 end
