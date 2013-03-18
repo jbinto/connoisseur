0fec184605c377339a96351f9492f2b05d83bb76
Jesse Buchanan <jbinto@jbinto.ca>
2013-03-18 10:50:12 -0400

    create HtmlGenerator class

---
+++ html_generator.rb
@@ -0,0 +1,10 @@
+# note: Style guide dictates not calling this class HTMLGenerator.
+class HtmlGenerator
+  def show
+    puts "Action: show"
+  end
+
+  def index
+    puts "Action: index"
+  end
+end
--- router.rb
+++ router.rb
@@ -1,3 +1,5 @@
+require_relative 'html_generator'
+
 # We need to be able to access parameters passed into the app.
 # Any Ruby app can be passed parameters.
 # It's up to the program to decide how (or even if) to respond.
@@ -6,11 +8,11 @@ if ARGV.empty?
 	puts "Usage: [args]"
 else
 	puts "Arguments passed in: #{ARGV.join(" ")}"
-
+  generator = HtmlGenerator.new
 	if ARGV[0] == "index"
-    puts "Action: index"
+    generator.index
 	elsif ARGV[0] == "show"
-    puts "Action: show"
+    generator.show
 	else
 		puts "Unrecognized action. Please refer to USAGE."
 	end
