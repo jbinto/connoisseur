55a1cc7d10c179e607356af14e4e960fb1437a50
Jesse Buchanan <jbinto@jbinto.ca>
2013-03-18 11:00:51 -0400

    Add print_header and print_footer methods

--- html_generator.rb
+++ html_generator.rb
@@ -1,10 +1,32 @@
 # note: Style guide dictates not calling this class HTMLGenerator.
+
 class HtmlGenerator
   def show
+    print_header
     puts "Action: show"
+    print_footer
   end
 
   def index
+    print_header
     puts "Action: index"
+    print_footer
+  end
+
+  # Consider that some HTML will be common to all documents, so
+  # let's write some shared methods for these.
+
+  def print_header
+    puts "<!doctype html>"
+    puts "<html>"
+    puts "  <head>"
+    puts "    <title>Connoisseur</title>"
+    puts "  </head>"
+    puts "  <body>"
+  end
+    
+  def print_footer
+    puts "  </body>"
+    puts "</html>"
   end
 end
